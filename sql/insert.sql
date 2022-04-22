e013ac83f06b374937fa9eed397e7e8c0bfec3d5
Fidel Brea <55228730+fidelbrea@users.noreply.github.com>
2022-04-22 22:01:35 +0200

    Add files via upload

---
+++ sql/insert.sql
@@ -0,0 +1,15 @@
+USE `alarma` ;
+
+INSERT INTO Usuario (Email,Alias,Es_administrador) VALUES ('alias@nomail.com','Alias',true);
+
+INSERT INTO Sensor (Id,Alias) VALUES (1,'Sensor 01');
+INSERT INTO Sensor (Id,Alias) VALUES (2,'Sensor 02');
+INSERT INTO Sensor (Id,Alias) VALUES (3,'Sensor 03');
+INSERT INTO Sensor (Id,Alias) VALUES (4,'Sensor 04');
+INSERT INTO Sensor (Id,Alias) VALUES (5,'Sensor 05');
+INSERT INTO Sensor (Id,Alias) VALUES (6,'Sensor 06');
+INSERT INTO Sensor (Id,Alias) VALUES (7,'Sensor 07');
+INSERT INTO Sensor (Id,Alias) VALUES (8,'Sensor 08');
+INSERT INTO Sensor (Id,Alias) VALUES (9,'Sensor 09');
+INSERT INTO Sensor (Id,Alias) VALUES (10,'Sensor 10');
+INSERT INTO Sensor (Id,Alias) VALUES (11,'Sensor 11');
---
+++ sql/script.sql
@@ -0,0 +1,47 @@
+DROP SCHEMA IF EXISTS `alarma`;
+CREATE SCHEMA IF NOT EXISTS `alarma` DEFAULT CHARACTER SET utf8;
+USE `alarma`;
+
+DROP TABLE IF EXISTS `alarma`.`Dispara`;
+DROP TABLE IF EXISTS `alarma`.`Usuario`;
+DROP TABLE IF EXISTS `alarma`.`Evento`;
+DROP TABLE IF EXISTS `alarma`.`Sensor`;
+DROP TABLE IF EXISTS `alarma`.`Camara`;
+
+CREATE TABLE IF NOT EXISTS `Usuario`(
+  `Email`            VARCHAR(100) NOT NULL PRIMARY KEY,
+  `Alias`            VARCHAR(20) NOT NULL UNIQUE,
+  `Es_administrador` BOOLEAN DEFAULT FALSE,
+  `Token`            VARCHAR(255),
+  `Tag_RFID`         INT,
+  `Codigo`           VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS `Evento`(
+  `Id`               INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `Timestamp`        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Descripcion`      VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `Sensor`(
+  `Id`               INT NOT NULL PRIMARY KEY,
+  `Alias`            VARCHAR(20) NOT NULL UNIQUE,
+  `Habilitado`       BOOLEAN DEFAULT FALSE,
+  `Retardado`        BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS `Camara`(
+  `Alias`            VARCHAR(20) NOT NULL PRIMARY KEY,
+  `Uri`              VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `Dispara`(
+  `Id_sensor_dispara`      INT NOT NULL,
+  `Alias_camara_disparada` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`Id_sensor_dispara`,`Alias_camara_disparada`)
+);
+
+ALTER TABLE `alarma`.`Dispara`
+  ADD FOREIGN KEY (`Id_sensor_dispara`) REFERENCES `alarma`.`Sensor` (`Id`),
+  ADD FOREIGN KEY (`Alias_camara_disparada`) REFERENCES `alarma`.`Camara` (`Alias`) ON UPDATE CASCADE;
+
